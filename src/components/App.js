8940d856c2e00dd1e1e7977298da707fcac0e8a8
quickheaven <quckheaven@gmail.com>
2020-01-03 11:48:24 -0500

    React App Template

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    state = { displayBio: false };
+
+    toggleDisplayBio = () => {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
+    render() {
+        return (
+            <div>
+                React App
+            </div>)
+    }
+}
+
+
+export default App;
